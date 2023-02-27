dae5ae7d1e7563f023663b621d01859721ee6437
ruslan21111 <rteimurov211@gmail.com>
2023-02-27 18:37:44 -0500

    Added toasts

--- app/src/main/java/com/example/filmsearch/MainActivity.kt
+++ app/src/main/java/com/example/filmsearch/MainActivity.kt
@@ -1,9 +1,21 @@
-package com.example.filmsearch
+package com.example.myapplication
 
+import android.content.ClipData.Item
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomappbar.BottomAppBarTopEdgeTreatment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenu
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,35 +23,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-   initMenuButtons()
 
 
-    }
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
 
-        private fun initMenuButtons() {
-            button1.setOnClickListener {
-                Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Watch Later", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Library", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
             }
-
-            button2.setOnClickListener {
-                Toast.makeText(this, "ИЗБРАННОЕ", Toast.LENGTH_SHORT).show()
-            }
-            button3.setOnClickListener {
-                Toast.makeText(this, "ПОСМОТРЕТЬ ПОЗЖЕ", Toast.LENGTH_SHORT).show()
-            }
-            button4.setOnClickListener {
-                Toast.makeText(this, "ПОДБОРКИ", Toast.LENGTH_SHORT).show()
-
-            }
-            button5.setOnClickListener {
-                Toast.makeText(this, "НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
-
-            }
-
         }
-    }
 
+
+
+        val topappbar: MaterialToolbar = findViewById(R.id.topAppBar)
+        topappbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.share -> {
+                    Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.search -> {
+                    Toast.makeText(this, "search", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+}
 
 
 
